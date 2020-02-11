700eb4f7952b7abdae27aaf63d1901be8574afd2
Denis Roenko <denis@roenko.ru>
2020-02-11 12:53:23 +0300

    Initial Commit

---
+++ FloatingTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FloatingTabBar
+//
+//  Created by Роенко Денис on 11.02.2020.
+//  Copyright © 2020 Denis Roenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
