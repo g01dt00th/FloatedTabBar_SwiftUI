bde94c28db2f1b8c6410173a97b79c21e03fcf0b
Denis Roenko <denis@roenko.ru>
2020-02-15 22:07:03 +0000

    first commit

---
+++ FloatingTabBar/FloatingTabbar.swift
@@ -0,0 +1,21 @@
+//
+//  FloatingTabbar.swift
+//  FloatingTabBar
+//
+//  Created by Роенко Денис on 11.02.2020.
+//  Copyright © 2020 Denis Roenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct FloatingTabbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FloatingTabbar_Previews: PreviewProvider {
+    static var previews: some View {
+        FloatingTabbar()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# FloatedTabBar_SwiftUI
