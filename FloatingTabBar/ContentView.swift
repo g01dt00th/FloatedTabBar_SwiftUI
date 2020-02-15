7eff532c15e3e16bef768576557e89155aba0f95
Denis Roenko <denis@roenko.ru>
2020-02-15 22:11:09 +0000

    1

--- FloatingTabBar/ContentView.swift
+++ FloatingTabBar/ContentView.swift
@@ -9,8 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selected = 0
+    @State var expand = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .bottom){
+            VStack {
+                if self.selected == 0 {
+                    NavigationView {
+                        ScrollView(.vertical, showsIndicators: false) {
+                            VStack(spacing: 15){
+                                ForEach(1...7, id:\.self) { i in
+                                    Image("\(i)").resizable().frame(height: 250).cornerRadius(15).shadow(radius: 10)
+
+                                }
+                            }.padding()
+                                
+                        }.navigationBarTitle("Home", displayMode: .inline)
+                        .onTapGesture {}
+                        .onLongPressGesture(minimumDuration: 1000000, maximumDistance: 1000, pressing: {pressing in
+                                if pressing && self.expand { self.expand.toggle() }
+                        }, perform: {})
+
+                    }.onAppear(perform: {if self.expand { self.expand.toggle() } })
+
+                } else if self.selected == 1 {
+                    GeometryReader {_ in
+                        Text("Wishlist") }
+                } else {
+                    GeometryReader {_ in
+                        Text("Cart") }
+                }
+            }.background(Color("Color").edgesIgnoringSafeArea(.all))
+                
+            FloatingTabbar(selected: self.$selected, expand: self.$expand)
+        }
     }
 }
 
--- FloatingTabBar/FloatingTabbar.swift
+++ FloatingTabBar/FloatingTabbar.swift
@@ -9,13 +9,62 @@
 import SwiftUI
 
 struct FloatingTabbar: View {
+    @Binding var selected: Int
+    @Binding var expand: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        
+        HStack {
+            Spacer(minLength: 0)
 
-struct FloatingTabbar_Previews: PreviewProvider {
-    static var previews: some View {
-        FloatingTabbar()
+            HStack {
+                if self.expand {
+                    Button(action: {
+                        //action here
+                        self.selected = 0
+                    }) {
+                        Image(systemName: "house").foregroundColor(self.selected == 0 ? .black : .gray ).padding(.horizontal)
+                    }
+                    Spacer(minLength: 15)
+                    Button(action: {
+                        //action here
+                        self.selected = 1
+                    }) {
+                        Image(systemName: "hand.thumbsup").foregroundColor(self.selected == 1 ? .black : .gray ).padding(.horizontal)
+                    }
+                    Spacer(minLength: 15)
+                    Button(action: {
+                        //action here
+                        self.selected = 2
+                    }) {
+                        Image(systemName: "cart").foregroundColor(self.selected == 2 ? .black : .gray ).padding(.horizontal)
+                    }
+                    Spacer(minLength: 15)
+                    Button(action: {
+                        //action here
+                        self.expand.toggle()
+                    }) {
+                        Image(systemName: "arrow.right").foregroundColor(self.selected == 3 ? .black : .gray ).padding(.horizontal)
+                    }
+
+                } else {
+                   
+                    Button(action: {
+                        // action here
+                        self.expand.toggle()
+                    }) {
+                        Image(systemName: "arrow.left").foregroundColor(.black).padding()
+                    }
+                }
+            }.padding(.vertical, self.expand ? 20 : 8)
+            .padding(.horizontal, self.expand ? 35 : 8)
+            .background(Color("TabBackColor"))
+            .clipShape(Capsule())
+            .padding(22)
+            .animation(.interactiveSpring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6))
+                
+        }
+
     }
+        
 }
